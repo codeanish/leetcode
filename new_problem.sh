9b264b02c896a6523980185d4c800495e3a48297
codeanish <anish@codeanish.com>
2023-10-07 21:56:21 -0700

    Updating new problem creation script

--- new_problem.sh
+++ new_problem.sh
@@ -5,17 +5,15 @@ if [ -z "$PROJECT_NAME" ]
 then 
     echo "Need to specify project name"
 else 
-    poetry new $PROJECT_NAME --quiet
+    poetry new $PROJECT_NAME --quiet --name src
     cd $PROJECT_NAME
     poetry add pytest --group dev
     poetry install
 
     mkdir .vscode
     cp ../setup/base_vscode_settings.json .vscode/settings.json
-    cp ../setup/main.py $PROJECT_NAME/main.py
+    cp ../setup/main.py src/main.py
     cp ../setup/test_main.py tests/test_main.py
 
-    # This is MacOS specific. On linux, you can omit the '' at the beginning
-    sed -i '' "s/PROJECT_NAME/$PROJECT_NAME/" tests/test_main.py
     code .
 fi
--- setup/test_main.py
+++ setup/test_main.py
@@ -1,4 +1,4 @@
-from PROJECT_NAME import main
+from src import main
 
 def test_hello():
     assert main.hello() == "Hello"
