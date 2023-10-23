d0e0bc9b762cb9698accb148822e07ab364df65e
codeanish <anish@codeanish.com>
2023-10-23 08:13:42 -0700

    102. Binary tree order traversal

--- 102_binary_tree_level_order_traversal/src/main.py
+++ 102_binary_tree_level_order_traversal/src/main.py
@@ -1,2 +1,40 @@
+from typing import List, Optional
+
+
 def hello():
     return ("Hello")
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        # Take first level and add the root value
+        if root:
+            result.append([root.val])
+            
+            level = []
+            # Get the second level nodes
+            if root.left:
+                level.append(root.left)
+            if root.right:
+                level.append(root.right)
+            
+            # For each value in the nodes at this level, add them to the result
+            while len(level) > 0:
+                level_array = []
+                new_level = []
+                for node in level:
+                    level_array.append(node.val)
+                    if node.left:
+                        new_level.append(node.left)
+                    if node.right:
+                        new_level.append(node.right)
+                result.append(level_array)
+                level = new_level
+                
+        return result
--- 102_binary_tree_level_order_traversal/tests/test_main.py
+++ 102_binary_tree_level_order_traversal/tests/test_main.py
@@ -2,3 +2,41 @@ from src import main
 
 def test_hello():
     assert main.hello() == "Hello"
+
+def test_level_order_example():
+    t5 = main.TreeNode(7, None, None)
+    t4 = main.TreeNode(15, None, None)
+    t3 = main.TreeNode(20, t4, t5)
+    t2 = main.TreeNode(9, None, None)
+    t1 = main.TreeNode(3, t2, t3)
+    sol = main.Solution()
+    result = sol.levelOrder(t1)
+    assert len(result) == 3
+    assert result[0][0] == 3
+    assert result[1][0] == 9
+    assert result[1][1] == 20
+    assert result[2][0] == 15
+    assert result[2][1] == 7
+
+def test_level_order_2_levels():
+    t3 = main.TreeNode(20, None, None)
+    t2 = main.TreeNode(9, None, None)
+    t1 = main.TreeNode(1, t2, t3)
+    sol = main.Solution()
+    result = sol.levelOrder(t1)
+    assert len(result) == 2
+    assert result[0][0] == 1
+    assert result[1][0] == 9
+    assert result[1][1] == 20
+
+def test_level_order_single_node():
+    t1 = main.TreeNode(1,None, None)
+    sol = main.Solution()
+    result = sol.levelOrder(t1)
+    assert len(result) == 1
+    assert result[0][0] == 1
+
+def test_level_order_null_node():
+    sol = main.Solution()
+    result = sol.levelOrder(None)
+    assert len(result) == 0
