60ea1d405ebcf5e9dc7a49f76b4f1277a184486c
codeanish <anish@codeanish.com>
2024-01-01 15:32:58 -0800

    Add iterative BFS and DFS

--- 104_maximum_depth_of_binary_tree/src/main.py
+++ 104_maximum_depth_of_binary_tree/src/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 
@@ -11,14 +12,43 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root: 
-            if root.left is None and root.right is None:
-                return 1
-            else:
-                return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        return 0
+    # Recursive DFS
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     if root: 
+    #         if root.left is None and root.right is None:
+    #             return 1
+    #         else:
+    #             return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    #     return 0
+
+    # Iterative BFS
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     if not root:
+    #         return 0
+    #     level = 0
+    #     q = deque([root])
+    #     while q:
+    #         for i in range(len(q)):
+    #             node = q.popleft()
+    #             if node.left:
+    #                 q.append(node.left)
+    #             if node.right:
+    #                 q.append(node.right)
+    #         level += 1
+    #     return level
     
+    # Iterative DFS
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root,1]]
+        result = 0
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                result = max(result, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return result
 
 if __name__ == "__main__":
     sol = Solution()
